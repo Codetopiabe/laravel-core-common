caf6a329dc11b2a44e7d352c61d51f3a268bbeea
Frederick Willems <fre.willems@thecomet.be>
2020-06-18 21:13:52 +0200

    NO-KEY: Optimize dependencies.

--- src/Domain/UuidModel.php
+++ src/Domain/UuidModel.php
@@ -8,12 +8,6 @@ class UuidModel extends Model
 {
     use GeneratesUuid;
 
-    // Not doing this for now because of route
-    // binding issues.Let's verify the performance hit first.
-    // protected $casts = [
-    //     'uuid' => EfficientUuid::class
-    // ];
-
     public function getRouteKeyName(): string
     {
         return 'uuid';
